c7da466be63ff135a95c3e47f74fdd0d29d7643f
Breathedude <rachithabayar@gmail.com>
2019-03-23 21:10:55 +0530

    Need to add design

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,6 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {'posts': Post.author})
+   # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')---error here.
+    #return render(request, 'blog/post_list.html', {'posts': posts})
+    #Gives no objects for Post!
